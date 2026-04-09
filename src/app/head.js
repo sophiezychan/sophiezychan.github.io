51ffe84c7a67f43e6b444f63ae0006ebfbeef6f3
Sophie Chan <sophiechan103@gmail.com>
2026-04-08 21:23:21 -0400

    try google search console indexing

---
+++ src/app/head.js
@@ -0,0 +1,12 @@
+// src/app/head.js
+export default function Head() {
+  return (
+    <>
+      <title>Sophie Chan</title>
+      <meta
+        name="google-site-verification"
+        content="=0zBu8UgNYDkIFTkLP2OQkcR7_mQNyQ8zM-DVu_J8MOc"
+      />
+    </>
+  );
+}
