a0c7f8931616730412ba37fbd4ac73d8e1533c19
Sophie Chan <sophiechan103@gmail.com>
2026-04-08 21:36:34 -0400

    try google indexing again

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,14 +8,17 @@ const redhat = Red_Hat_Text({ subsets: ["latin"] });
 export const metadata = {
   title: "Sophie Chan",
   description: "Sophie personal website",
+  verification: {
+    google: "0zBu8UgNYDkIFTkLP2OQkcR7_mQNyQ8zM-DVu_J8MOc",
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-          <link rel="icon" href="./logo.svg" />
-        </head>
+        <link rel="icon" href="./logo.svg" />
+      </head>
       <body className={redhat.className}>
         <Navbar />
         {children}
